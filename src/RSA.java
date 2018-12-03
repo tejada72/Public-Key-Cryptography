eaa73b78c34b22104701b02aec25401a6d7966d9
Unknown <wesleyaegaz@gmail.com>
2018-12-03 09:40:52 -0500

    Added stub for inverse method and completed modPower method

--- src/RSA.java
+++ src/RSA.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.math.*;
 import java.util.*;	// Random number generator
+import java.lang.Long;
 
 public class RSA
 {
@@ -31,7 +32,50 @@ public class RSA
         //System.out.println ("Alice decodes and reads: " + Alice.decrypt (cipher));
     }
 
-
+   /*
+   @author:Wes Holman
+   */
+   public static long inverse(long e, long m){
+   /*Find the multiplicative inverse of a long int
+Returns:
+The inverse of e, mod m. Uses the extended Eulidean Algorithm*/
+      return 0;
+   }
+   
+   /*
+   @author:Wes Holman
+   */
+   public static long modPower(long b, long p, long m){
+      /*Raise a number, b, to a power, p, modulo m
+      Returns:
+      bp mod m*/
+      /*
+      Decompose p into powers of 2 through repeated division while p>0
+      keep track of first 1 digit, then start keeping track of 0s
+      create an array of the same size
+      first entry is b
+      next n-1 entries are arr[i-1]*arr[i-1] % m
+      REMEMBER ORDER
+      result = 1
+      for every element of numbers, multiply it by corresponding 0 or 1, multiply with result, mod m
+      return result
+      */
+      long result = 1;
+      String bits = Long.toBinaryString(p);
+      bits = bits.substring(bits.indexOf('1'));
+      long[] powers = new long[bits.length()];
+      powers[0] = b;
+      for(int i = 1; i < powers.length; i++){
+         powers[i] = (powers[i-1]*powers[i-1]) % m;
+      }
+      for(int i = bits.length(); i >= 0; i--){
+         if(bits.charAt(i) == '1'){
+            result *= powers[bits.length() - i];
+            result = result % m;
+         }
+      }
+      return result;
+   }
 
 
 // ..........  place class methods here
